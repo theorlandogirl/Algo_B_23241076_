88ccdbb50a5168ffb0aecaff05105180124b3912
theorlandogirl <146050758+theorlandogirl@users.noreply.github.com>
2023-10-31 13:24:28 +0800

    kuliah algo31oktober 2023

---
+++ 01-programjavapertama/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Program Biodata Mahasiswa
+        String nama;
+        String nim; 
+        String tgl_lahir;
+        String alamat;
+        float berat_badan;
+        float tinggi_badan;
+
+
+        //mengisi variabel
+        nama = "pitri ziadatul jannah";
+        nim = "23241076";
+        tgl_lahir = "13 november 2004";
+        alamat = "jalan adi sucipto";
+        berat_badan = 40f;
+        tinggi_badan = 153f;
+
+
+        //cetak biodata
+        System.out.println("........................");
+        System.out.println("Nama: " + nama);
+        System.out.println("Nim: " + nim);
+        System.out.println("Tanggal Lahir: " + tgl_lahir);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Berat Badan: " + berat_badan);
+        System.out.println("Tinggi Badan: " + tinggi_badan);
+        System.out.println(".........................");
+        
+
+
+
+    }
+}

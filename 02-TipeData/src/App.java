8e7e52d223b66b6e40497526c4cd7068a779d4ce
theorlandogirl <146050758+theorlandogirl@users.noreply.github.com>
2023-11-28 12:55:36 +0800

    28-11-23 TIPE DATA

---
+++ 02-TipeData/src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+       // tipe data di java 
+       // integer, byte, short, long, double, float, char, boolean
+
+       // integer (satuan)
+       int i = 10;
+       System.out.println("=====INTEGER=====");
+       System.out.println("Nilai Integer i =" + i);
+       System.out.println("Nilai MAX  :" + Integer. MAX_VALUE);
+       System.out.println("Nilai MIN  :" + Integer.MIN_VALUE);
+       System.out.println("Besar Integer" + Integer.SIZE + "bin");
+
+       //byte
+       byte b = 10;
+       System.out.println("=====BYTE=====");
+       System.out.println("Nilai Byte b =" + b);
+       System.out.println("Nilai MAX  :" + Byte. MAX_VALUE);
+       System.out.println("Nilai MIN  :" + Byte.MIN_VALUE);
+       System.out.println("Besar Byte" + Byte.SIZE + "bin");
+
+       //char
+       char c = 10;
+       System.out.println("=====CHAR=====");
+       System.out.println("Nilai Char c =" + c);
+       System.out.println("Nilai MAX  :" + Integer.SIZE + "bit");
+
+       //boolean
+       Int bl = 10;
+       System.out.println("=====BOOLEAN=====");
+       System.out.println("Nilai Boolean bl =" + bl);
+       System.out.println("Nilai Boolean  :" + Byte.SIZE + "bit");
+
+       
+
+
+       
+    }
+}
